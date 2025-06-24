3c49c718621a1512fd993f41d760be1c994bc3ba
Jose Alejandro Gomez Castro <josegomez.dev@gmail.com>
2025-06-24 11:56:20 -0600

    increase securities by implement env vars

--- firebase.ts
+++ firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDLzTaGe_WCw9RVdutz4Mqx-ViQrL_to9Y",
-  authDomain: "blockbeats-ee6d3.firebaseapp.com",
-  projectId: "blockbeats-ee6d3",
-  storageBucket: "blockbeats-ee6d3.firebasestorage.app",
-  messagingSenderId: "380764586846",
-  appId: "1:380764586846:web:7285f377aa1d29103d85cf",
-  measurementId: "G-018RENMEBY"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
